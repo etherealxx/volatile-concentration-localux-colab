9ef33885e509cb523cb7f7d9354b7a81a0c50fe1
Etherealxx <gwathon3@gmail.com>
2023-04-21 22:01:01 +0700

    For downloader

---
+++ runaria2.py
@@ -0,0 +1,16 @@
+import os, pickle, subprocess
+
+arialines = None
+
+if os.path.exists('/content/arialist.pkl'):
+  with open('/content/arialist.pkl', 'rb') as f:
+      arialines = pickle.load(f)
+
+if arialines:
+  for line in arialines:
+    if not '4x-UltraSharp.pth' in line:
+      ariaexecline = line[2:].replace('\\n",', '').replace('/content/stable-diffusion-webui', '/content/volatile-concentration-localux')
+      try:
+        subprocess.run(ariaexecline, shell=True, check=True)
+      except Exception as e:
+          print("Exception: " + str(e))
