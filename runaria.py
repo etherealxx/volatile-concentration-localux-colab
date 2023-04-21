599a2a3215223b6fc67dd28ed6721d08c8ade01b
Etherealxx <gwathon3@gmail.com>
2023-04-21 11:07:12 +0700

    scripts needed

---
+++ runaria.py
@@ -0,0 +1,12 @@
+import pickle, subprocess
+
+with open('/content/arialist.pkl', 'rb') as f:
+    arialines = pickle.load(f)
+    
+for line in arialines:
+  if not '4x-UltraSharp.pth' in line:
+    ariaexecline = line[2:].replace('\\n",', '')
+    try:
+      subprocess.run(ariaexecline, shell=True, check=True)
+    except Exception as e:
+        print("Exception: " + str(e))
