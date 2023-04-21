0938fdf2397c0ca99a45d921979f60b8ff37190f
Etherealxx <gwathon3@gmail.com>
2023-04-21 11:13:17 +0700

    rename to VCL

--- runaria.py
+++ runaria.py
@@ -5,7 +5,7 @@ with open('/content/arialist.pkl', 'rb') as f:
     
 for line in arialines:
   if not '4x-UltraSharp.pth' in line:
-    ariaexecline = line[2:].replace('\\n",', '')
+    ariaexecline = line[2:].replace('\\n",', '').replace('/content/stable-diffusion-webui', '/content/volatile-concentration-localux')
     try:
       subprocess.run(ariaexecline, shell=True, check=True)
     except Exception as e:
