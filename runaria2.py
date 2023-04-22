94ae1a5c4e9867c956b1b6113679f51034b0e252
Etherealxx <gwathon3@gmail.com>
2023-04-23 05:10:03 +0700

    green color on runaria

--- runaria2.py
+++ runaria2.py
@@ -11,6 +11,8 @@ if arialines:
     if not '4x-UltraSharp.pth' in line:
       ariaexecline = line[2:].replace('\\n",', '').replace('/content/stable-diffusion-webui', '/content/volatile-concentration-localux')
       try:
+        print("[1;32m" + line)
+        print('[0m')
         subprocess.run(ariaexecline, shell=True, check=True)
       except Exception as e:
           print("Exception: " + str(e))
