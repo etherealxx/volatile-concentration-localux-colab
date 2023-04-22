bfdd0e524d8bcc3001363a43de3c43de571f3343
Etherealxx <gwathon3@gmail.com>
2023-04-23 06:21:24 +0700

    controlnet mode

--- runaria2.py
+++ runaria2.py
@@ -1,18 +1,31 @@
 import os, pickle, subprocess
 
 arialines = None
+downloadcnet = False
 
 if os.path.exists('/content/arialist.pkl'):
   with open('/content/arialist.pkl', 'rb') as f:
       arialines = pickle.load(f)
 
+if os.path.exists('/content/colaboptions.pkl'):
+  with open('/content/colaboptions.pkl', 'rb') as f:
+      downloadcnet = pickle.load(f)
+
+def subprocessing(execline):
+  try:
+    print("[1;32m" + execline)
+    print('[0m')
+    subprocess.run(execline, shell=True, check=True)
+  except Exception as e:
+      print("Exception: " + str(e))
+
 if arialines:
   for line in arialines:
     if not '4x-UltraSharp.pth' in line:
       ariaexecline = line[2:].replace('\\n",', '').replace('/content/stable-diffusion-webui', '/content/volatile-concentration-localux')
-      try:
-        print("[1;32m" + ariaexecline)
-        print('[0m')
-        subprocess.run(ariaexecline, shell=True, check=True)
-      except Exception as e:
-          print("Exception: " + str(e))
+      if 'ControlNet' in ariaexecline:
+         if downloadcnet:
+            subprocessing(ariaexecline)
+      else:
+        subprocessing(ariaexecline)
+
